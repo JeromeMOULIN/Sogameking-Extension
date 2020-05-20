fd858c3c410f81728d34315a75f7df9de6bbf41a
JeromeMOULIN <j.moulin89@laposte.net>
2020-05-21 01:26:54 +0200

    Extension Sogameking for google chrome

---
+++ js/function.js
@@ -0,0 +1,8 @@
+//notifie quand l'utilisateur met a jour l'extention
+chrome.runtime.onInstalled.addListener(function(){
+    alert("Merci d'avoir mis à jour ou installé mon extention !");
+});
+
+
+
+
